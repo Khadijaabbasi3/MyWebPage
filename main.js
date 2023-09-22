78172fb02f4c1269928e0d2d1ac902aa85d40289
Khadijaabbasi3 <137084461+Khadijaabbasi3@users.noreply.github.com>
2023-09-22 18:05:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var swiper = new Swiper('.swiper-container', {
+  // Optional: Customize Swiper options here
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+});
